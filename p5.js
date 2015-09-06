b67891f78788f692c0bd9938f04a472c8fbf4573
Falcon <jfalcon95@outlook.com>
2015-09-06 10:38:15 -0500

    Exercise 5 v. 1

--- p5.js
+++ p5.js
@@ -2,10 +2,12 @@ fs = require('fs');
 
 fs.readdir(process.argv[2], function(err, files){
    if(!err){
-      console.log(files);
+	for(var i=0; i<files.length; i++){
+	  if(files[i].split('.').pop() === process.argv[3] && files[i].indexOf('.') != -1)
+	    console.log(files[i]);
+	}  	
    }
    else{
       throw err;
    }
 });
-
